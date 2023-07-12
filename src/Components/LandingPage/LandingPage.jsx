efe1ce2011b58db9110204463998d34f3c6fd424
Carlos Collado <carlose0808@hotmail.com>
2023-07-12 00:19:37 -0400

    React Router Added

---
+++ src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,12 @@
+import "./LandingPage.css";
+import Navbar from "./NavBar/Navbar";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/Components/LandingPage/NavBar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+};
+
+export default Navbar;
