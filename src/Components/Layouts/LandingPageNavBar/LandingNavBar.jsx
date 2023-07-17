6ca5729dd666efdf47c198994c5293a96abe0886
Carlos Collado <carlose0808@hotmail.com>
2023-07-17 17:54:06 -0400

    Fixed LandingNavbar Bug

--- src/Components/Layouts/LandingPageNavBar/LandingNavBar.jsx
+++ src/Components/Layouts/LandingPageNavBar/LandingNavBar.jsx
@@ -1,15 +1,23 @@
 import "./LandingNavBar.css";
 import logo from "../../../images/LogoFoodForecast.png";
 import { Link, Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const LandingNavbar = () => {
+  const location = useLocation();
+
+  const handleClick = (id) => {
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div>
       <nav className="bg-white w-full border-b border-gray-200">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="#" className="flex items-center">
+          <Link to="/" className="flex items-center">
             <img src={logo} className="h-8 mr-3" alt="Flowbite Logo" />
-          </a>
+          </Link>
           <div className="flex md:order-2">
             <Link to="/login">
               <button
@@ -32,40 +40,46 @@ const LandingNavbar = () => {
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-              <li>
-                <Link
-                  to="/#home"
-                  className="block py-2 pl-3 pr-4 text-neutral-800 bg-blue-700 rounded bg-transparent hover:text-green-700 p-0"
-                >
-                  Inicio
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/#about-us"
-                  className="block py-2 pl-3 pr-4 text-neutral-800 bg-blue-700 rounded bg-transparent hover:text-green-700 p-0"
-                >
-                  Sobre Nosotros
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/#subscription"
-                  className="block py-2 pl-3 pr-4 text-neutral-800 bg-blue-700 rounded bg-transparent hover:text-green-700 p-0"
-                >
-                  Subscripcion
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/#faq"
-                  className="block py-2 pl-3 pr-4 text-neutral-800 bg-blue-700 rounded bg-transparent hover:text-green-700 p-0"
-                >
-                  FAQ
-                </Link>
-              </li>
-            </ul>
+            {location.pathname === "/" && (
+              <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+                <li>
+                  <Link
+                    to="/#home"
+                    onClick={() => handleClick("home")}
+                    className="block py-2 pl-3 pr-4 text-neutral-800 bg-blue-700 rounded bg-transparent hover:text-green-700 p-0"
+                  >
+                    Inicio
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/#about-us"
+                    onClick={() => handleClick("about-us")}
+                    className="block py-2 pl-3 pr-4 text-neutral-800 bg-blue-700 rounded bg-transparent hover:text-green-700 p-0"
+                  >
+                    Sobre Nosotros
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/#subscription"
+                    onClick={() => handleClick("subscription")}
+                    className="block py-2 pl-3 pr-4 text-neutral-800 bg-blue-700 rounded bg-transparent hover:text-green-700 p-0"
+                  >
+                    Subscripcion
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/#faq"
+                    onClick={() => handleClick("faq")}
+                    className="block py-2 pl-3 pr-4 text-neutral-800 bg-blue-700 rounded bg-transparent hover:text-green-700 p-0"
+                  >
+                    FAQ
+                  </Link>
+                </li>
+              </ul>
+            )}
           </div>
         </div>
       </nav>
