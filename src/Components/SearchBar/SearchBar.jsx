26de123c35b912698fcfe1aeb860a0845379ea01
Carlos Collado <carlose0808@hotmail.com>
2023-07-19 12:59:15 -0400

    Fix SearchBar Styling

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -30,17 +30,39 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="search-bar">
-      <input type="text" placeholder="Search" onChange={handleChange} />
-      {products.length > 0 && (
-        <ul className="dropdown-menu">
-          {products.map(product => (
-            <li key={product._id} className="dropdown-item">
-              <img src={product.imageUrl} alt={product.productName} />
-              <Link to={`/admin/product/${product._id}`}>
+    <div className="search-bar w-96">
+      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+        <svg
+          className="h-4 w-4 text-gray-500 dark:text-gray-400"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+          />
+        </svg>
+      </div>
+      <input
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+        type="text"
+        placeholder="Buscar..."
+        onChange={handleChange}
+      />
+      {products.length > 0 && searchTerm && (
+        <ul className="dropdown-menu rounded-lg border border-gray-300">
+          {products.slice(0, 6).map(product => (
+            <Link to={`/admin/product/${product._id}`}>
+              <li key={product._id} className="dropdown-item hover:bg-gray-400">
+                <img src={product.imageUrl} alt={product.productName} />
                 {product.productName}
-              </Link>
-            </li>
+              </li>
+            </Link>
           ))}
         </ul>
       )}
