60a41006508f401b5c2c42fef1e597737f67f287
Ricardo Arturo <49619270+Ricaidito@users.noreply.github.com>
2023-08-14 19:48:33 -0400

    Fixed url

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const url = "http://localhost:8000";
 
 const userLogin = (email, password) => {
-  return axios.post(`${url}/users/validate`, { email, password });
+  return axios.post(`${url}/users/login`, { email, password });
 };
 
 const getUserImage = id => {
