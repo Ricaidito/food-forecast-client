7513b053f3cd8a466e62e3f0b67d9300bca5e7b6
Carlos Collado <carlose0808@hotmail.com>
2023-09-27 16:42:37 -0400

    Fixed className

--- src/Components/CategoryFilter/CategoryFilter.jsx
+++ src/Components/CategoryFilter/CategoryFilter.jsx
@@ -6,7 +6,7 @@ const CategoryFilter = ({ switchCategory }) => {
   };
 
   return (
-    <div class="inline-flex w-2/4 rounded-md shadow-sm" role="group">
+    <div className="inline-flex w-2/4 rounded-md shadow-sm" role="group">
       <button
         className="rounded-l-lg border border-gray-200 bg-green-500 px-4 py-2 text-sm font-medium text-white hover:bg-green-400 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:ring-2 focus:ring-blue-700 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:text-white dark:focus:text-white dark:focus:ring-blue-500"
         onClick={() => handleClick(null)}
