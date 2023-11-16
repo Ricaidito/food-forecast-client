240f302995e1210a31e2b27d7555281f05e47175
Carlos Collado <carlose0808@hotmail.com>
2023-11-16 00:09:30 -0400

    Updated configuracion and added UserConfigContexts

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { UserProvider } from "./Contexts/UserContext.jsx";
 import { ProductProvider } from "./Contexts/ProductContext.jsx";
+import { UserConfigContextProvider } from "./Contexts/UserConfigContext.jsx";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
@@ -16,7 +17,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Elements stripe={stripePromise}>
       <UserProvider>
         <ProductProvider>
-          <App />
+          <UserConfigContextProvider>
+            <App />
+          </UserConfigContextProvider>
         </ProductProvider>
       </UserProvider>
     </Elements>
