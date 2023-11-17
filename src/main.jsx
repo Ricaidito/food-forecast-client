7aeeeeae44007bc635b3186182587bda3f1defc0
Carlos Collado <carlose0808@hotmail.com>
2023-11-17 01:16:40 -0400

    UserProductContext not working

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { UserProvider } from "./Contexts/UserContext.jsx";
 import { ProductProvider } from "./Contexts/ProductContext.jsx";
+import { UserProductProvider } from "./Contexts/UserProductContext.jsx";
 import { UserConfigContextProvider } from "./Contexts/UserConfigContext.jsx";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
@@ -17,9 +18,11 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <Elements stripe={stripePromise}>
       <UserProvider>
         <ProductProvider>
-          <UserConfigContextProvider>
-            <App />
-          </UserConfigContextProvider>
+          <UserProductProvider>
+            <UserConfigContextProvider>
+              <App />
+            </UserConfigContextProvider>
+          </UserProductProvider>
         </ProductProvider>
       </UserProvider>
     </Elements>
