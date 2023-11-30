94819b0fa91038b50b56b128dfd7b4195fd4fc29
Carlos Collado <carlose0808@hotmail.com>
2023-11-30 11:52:35 -0400

    Update deleteProduct function to include userID

--- src/Components/Admin/MyProducts/MyProducts.jsx
+++ src/Components/Admin/MyProducts/MyProducts.jsx
@@ -451,7 +451,7 @@ const MyProducts = () => {
                       </div>
                     )}
                     <button
-                      onClick={() => deleteProduct(product._id)}
+                      onClick={() => deleteProduct(userID, product._id)}
                       className=" text-red-500 hover:text-red-600"
                     >
                       <TrashIcon className="h-5 w-5" />
